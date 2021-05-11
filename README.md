d86e780485f32c875a87fac2902c7ce5f261d697
Sumanth Nacharam <nacharamsumanth@gmail.com>
2021-05-10 21:27:53 -0500

    Creating initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Readme file created by Sumanth Nacharam. This project will print different statistics on the data provided in the form of array and it will sort the array from largest to smallest.
---
+++ stats.h
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.h> 
+ * @brief <This header consists of declarations of the functions and parameters they take>
+ *
+ * 
+ *
+ * @author <Sumanth Nacharam>
+ * @date <05/06/2021>
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+
+
+
+
+#endif /* __STATS_H__ */
